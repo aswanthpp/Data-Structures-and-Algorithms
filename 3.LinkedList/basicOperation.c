3a43927969c741bbd2b1872f42f36c33de08f78d
ASWANTH P P <ppaswanth3@gmail.com>
2018-01-17 02:10:18 +0530

    linked list added

--- 3.LinkedList/basicOperation.c
+++ 3.LinkedList/basicOperation.c
@@ -6,6 +6,32 @@
 */
 #include<iostream>
 using namespace std;
+struct Node{
+	int data;
+	struct Node *next;
+}*head;
+void insert(struct Node *node,int key){
+	if(node==NULL){
+		node=new Node;
+	}
+}
 int main(){
-	int 
+	int x;
+	xx:cout<<"\n1.Insert\n2.Delete\n3.Display\n4.Exit\n\tEnter Option";
+	cin>>x;
+	switch(x){
+		case 1: cout<<"\n\t\tEnter value :";
+			int y;
+			cin>>y;
+			insert(head,y);
+			goto xx;
+		case 2:cout<<"\nEnter positon to delete :";
+			cin>>y;
+			remove(head,y);
+			goto xx;
+		case 3:cout<<"\nCurrent Linked List : ";
+			display(head);
+			goto xx;
+		case 4:return 1;
+	}
 }
