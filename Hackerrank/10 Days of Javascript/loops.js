e74b6d279f41d22b20d912824970cf101999ba92
ASWANTH P P <ppaswanth3@gmail.com>
2019-07-03 20:53:57 +0530

    Javascript problems added

---
+++ Hackerrank/10 Days of Javascript/loops.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+   var i = 0;
+    while (i < s.length) {
+        // console.log("h\n");
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+            console.log(s[i]);
+        }
+        i++;
+    }
+    i = 0;
+    while (i < s.length) {
+        // console.log("h\n");
+        if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u') {
+            console.log(s[i]);
+        }
+        i++;
+    }
+}
+
